c5bd020ad134867c65a58f1d3b251a14093d728c
blackcolver88 <madokakhchimadi@gmail.com>
2025-02-23 19:42:24 +0100

    Added delete functionality for section layout

--- frontend/src/app/pages/editor-tree/editor-tree.component.ts
+++ frontend/src/app/pages/editor-tree/editor-tree.component.ts
@@ -37,6 +37,7 @@ import {PasswordConfigComponent} from '../../configurations/password-config/pass
 import { FormLayout } from '../../model/FormLayout';
 import { FormTemplateService } from '../../services/form-template.service';
 import { FormTemplate } from '../../model/FormTemplate';
+import { FormLayoutService } from '../../services/form-layout.service';
 
 export interface FoodNode {
   name: string;
@@ -46,7 +47,7 @@ export interface FoodNode {
 const TREE_DATA: FoodNode[] = [
   {
     name: 'Layout',
-    children: [{ name: 'Section' }],
+    children: [{ name: 'SECTION' }],
   },
   {
     name: 'Form',
@@ -91,7 +92,7 @@ export class EditorTreeComponent implements OnInit, OnDestroy {
   formTemplate!: FormTemplate;
 
   constructor(private route: ActivatedRoute, private cdr: ChangeDetectorRef,private library: FaIconLibrary,
-     private matDialog: MatDialog,private formTemplateService: FormTemplateService) {
+     private matDialog: MatDialog,private formTemplateService: FormTemplateService,private formLayoutService: FormLayoutService) {
     library.addIcons(faTrashAlt);
   }
 
@@ -234,10 +235,25 @@ export class EditorTreeComponent implements OnInit, OnDestroy {
 
   removeItem(item: any) {
     const index = this.editorItems.indexOf(item);
-    if (index > -1) {
+    if (index === -1) return;
+
+    // If the item has an ID, delete it via the API
+    if (item.id) {
+      this.formLayoutService.deleteFormLayout(item.id).subscribe({
+        next: () => {
+          this.editorItems.splice(index, 1);
+          console.log('Item deleted successfully:', item);
+        },
+        error: (err) => {
+          console.error('Error deleting item:', err);
+          // Handle error (e.g., show a notification)
+        }
+      });
+    } else {
+      // If the item has no ID, remove it locally
       this.editorItems.splice(index, 1);
+      console.log('Item removed locally:', item);
     }
-    console.log('Item removed:', item);
   }
 
   openDeleteDialog(): void {
