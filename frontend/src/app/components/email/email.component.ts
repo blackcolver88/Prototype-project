c208360f43589c0cab465d18c62c4b700a03a218
blackcolver88 <madokakhchimadi@gmail.com>
2025-03-07 14:41:04 +0100

    fixed the email label font style

--- frontend/src/app/components/email/email.component.ts
+++ frontend/src/app/components/email/email.component.ts
@@ -11,12 +11,9 @@ import { FormsModule } from '@angular/forms';
 })
 export class EmailComponent {
   @Input() label: string = 'Email';
-  @Input() labelPosition: 'top' | 'left' | 'right' | 'bottom' = 'top';
-  @Input() labelAlignment: 'left' | 'right' = 'left';
   @Input() type: 'text' | 'number' = 'text';
   @Input() textSize: number = 14;
   @Input() fontColor: string = '#000000';
-  @Input() fontFamily: string = 'Arial';
   @Input() email: string = '';
   @Input() placeholder: string = 'Enter your email';
 
