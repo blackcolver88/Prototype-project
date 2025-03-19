35c8e8d97a1ce38321922d575fb55b849fe7ca98
blackcolver88 <madokakhchimadi@gmail.com>
2025-03-19 16:20:02 +0100

    fixed radio button issue

--- frontend/src/app/components/radio-button/radio-button.component.ts
+++ frontend/src/app/components/radio-button/radio-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -14,15 +14,21 @@ interface RadioOption {
   standalone: true,
   imports: [CommonModule, FormsModule]
 })
-export class RadioButtonComponent {
+export class RadioButtonComponent implements OnInit {
   @Input() groupLabel: string = 'Radio Group';
   @Input() options: RadioOption[] = [];
-  @Input() name: string = 'radio';
+  @Input() name: string = '';
   @Input() isRequired: boolean = false;
   @Input() isDisabled: boolean = false;
   @Input() selectedOption: string = '';
 
   @Output() valueChange = new EventEmitter<string>();
+
+  ngOnInit() {
+    if (!this.name) {
+      this.name = `radio_${Math.random().toString(36).substr(2, 9)}`;
+    }
+  }
 
   onOptionChange(selectedValue: string) {
     this.selectedOption = selectedValue; // Make sure to update the internal state
--- frontend/src/app/configurations/radio-button-config/radio-button-config.component.ts
+++ frontend/src/app/configurations/radio-button-config/radio-button-config.component.ts
@@ -3,11 +3,13 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, FormArray, Validators } fr
 import { RadioButtonComponent } from '../../components/radio-button/radio-button.component';
 import { CommonModule } from '@angular/common';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+
 interface Option {
   label: string;
   value: string;
   checked: boolean;
 }
+
 @Component({
   selector: 'app-radio-button-config',
   templateUrl: './radio-button-config.component.html',
@@ -21,9 +23,8 @@ export class RadioButtonConfigComponent implements OnInit {
 
   constructor(private fb: FormBuilder, @Inject(DIALOG_DATA) public data: any) {
     this.radioForm = this.fb.group({
-      // groupLabel: ['Radio Group'],
-       title: ['', Validators.required],    
-      name: [''],
+      title: ['', Validators.required],
+      name: [`radio_${Math.random().toString(36).substr(2, 9)}`], // Generate a unique name
       isRequired: [false],
       isDisabled: [false],
       options: this.fb.array([])
@@ -32,12 +33,14 @@ export class RadioButtonConfigComponent implements OnInit {
     // Add default option
     this.addOption();
   }
+
   ngOnInit() {
     if (this.data && this.data.item && this.data.item.config) {
       this.radioForm.patchValue({
         title: this.data.item.config.title || '',
-        name: this.data.item.config.name || '',
-        isRequired: this.data.item.config.isRequired || false,  });
+        name: this.data.item.config.name || `radio_${Math.random().toString(36).substr(2, 9)}`, // Ensure unique name
+        isRequired: this.data.item.config.isRequired || false,
+      });
     }
   }
 
@@ -64,6 +67,7 @@ export class RadioButtonConfigComponent implements OnInit {
       type: 'RADIO_BUTTON',
       config: {
         label: formData.title,
+        name: formData.name, // Ensure the name is included in the config
         options: formData.options.map((option: Option) => ({
           label: option.label,
           value: option.value,
